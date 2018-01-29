634ec08457d10c34a4ca662c54dbb4de2bf4ad9d
xueyongjun <yongjun.xue@autozi.com>
2018-01-29 17:37:53 +0800

    前端为jsp页面

--- action/src/main/java/com/xue/test3/action/TestAction.java
+++ action/src/main/java/com/xue/test3/action/TestAction.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,15 +18,22 @@ public class TestAction {
 	private TestInterface testInterface;
 	
 	@RequestMapping("/test")
-	@ResponseBody
+//	@ResponseBody
 	public String receiveTask(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		@SuppressWarnings("unused")
 		String result=testInterface.getResult();
-		
-//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"/spring/spring-mvc.xml"});
-//		TestInterface o = (TestInterface)context.getBean("testInterface");
-		
 		System.out.println("test");
 		return "main";
-	}	
+	}
+	
+	@RequestMapping("/test1")
+//	@ResponseBody
+	public String test1(Model model) throws Exception {
+		String result=testInterface.getResult();
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"/spring/spring-mvc.xml"});
+//		TestInterface o = (TestInterface)context.getBean("testInterface");
+		model.addAttribute("result",result);  
+		return "jsp/mygoods";
+	}
+	
 }
